40507fb700736ebbbb0159de3c686765f4bde430
Alexander Rybin <alex_rybin@hotmail.com>
2020-01-14 22:57:25 +0300

    Add dict tests

--- conftest.py
+++ conftest.py
@@ -14,3 +14,13 @@ def list_for_tests():
 @pytest.fixture
 def set_for_tests():
     return {5, 4, 3, 'two', 'one', None}
+
+
+@pytest.fixture
+def dict_for_tests():
+    return {
+        'integer': 1,
+        'float': 0.1984,
+        'string': 'The quick brown fox jumps over the lazy dog',
+        'boolean': True
+    }
